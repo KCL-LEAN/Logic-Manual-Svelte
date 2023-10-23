2cbda5b105dfa96539e7e69f8d753891201f3353
Llnski <connor.duke.molinski@gmail.com>
2023-10-23 22:57:10 +0100

    it did not actually build correctly before, added html loader to webpack which I think changes the priority of loads or something

--- client/src/App.test.ts
+++
@@ -1,9 +0,0 @@
-//import { render, screen } from '@testing-library/react';
-//import EditorViewport from './EditorViewport.tsx';
-//
-//test('renders learn react link', () => {
-//  render(<EditorViewport/>);
-//  const linkElement = screen.getByText(/learn react/i);
-//  expect(linkElement).toBeInTheDocument();
-//});
-export {}
--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,6 @@
-<script>
-	export let name;
-</script>
-
 <main>
 	<h1>Hello {name}!</h1>
+    <h2>Wow</h2>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
@@ -28,3 +25,8 @@
 		}
 	}
 </style>
+
+<script>
+	export let name;
+</script>
+
