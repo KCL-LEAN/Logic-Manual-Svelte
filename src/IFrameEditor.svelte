db4bdb11d8ff7f66e98bb070cd31dd09737e4d47
Llnski <connor.duke.molinski@gmail.com>
2023-12-23 01:23:23 -0800

    Revert "current broken"
    This reverts commit a417866cd765b95bc0797f9063bee6a51da259cc.
    This is what I actually wanted to revert:)

--- src/IFrameEditor.svelte
+++ src/IFrameEditor.svelte
@@ -3,8 +3,9 @@
     const editorUrl = "https://leanprover-community.github.io/lean-web-editor/"
 
     export let editorText;
+    let frame;
      onMount(() => {
-        //frame.addEventListener('load', onLoad());
+        frame.addEventListener('load', onLoad());
       })
     function onLoad () {
         let loadHelper = async function () { setTimeout(async function () {
@@ -21,18 +22,10 @@
         }
         loadHelper();
     }
-    //window dimensions
-
-    let frame : HTMLIFrameElement;
-    frame.src = editorUrl;
-    frame.width = "300";
-    frame.height= "100";
-
     
-
 </script>
 <div class = "IFrameEditor">
-        <iframe bind:this={frame}></iframe>
+        <iframe bind:this={frame} src = "https://leanprover-community.github.io/lean-web-editor/" width = "300" height = "800"></iframe>
 </div>
 
 <style>
